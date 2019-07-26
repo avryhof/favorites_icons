4360a787b4753e6cf42bf48987bcdda9abebe65c
Amos Vryhof <amos@vryhofresearch.com>
2019-07-26 10:14:10 -0400

    Generate the manifest.json and link it

--- favorites_icons/templatetags/favorites_icons.py
+++ favorites_icons/templatetags/favorites_icons.py
@@ -8,7 +8,7 @@ from django import template
 from django.conf import settings
 from django.utils.safestring import mark_safe
 
-from .utils import generate_icons, url_join
+from .utils import generate_icons, url_join, generate_manifest
 
 register = template.Library()
 
@@ -16,6 +16,7 @@ register = template.Library()
 @register.simple_tag
 def touch_icons():
     generate_icons()
+    generate_manifest()
 
     static_root = getattr(settings, "STATIC_URL", False)
     icon_path = os.path.join(static_root, "favicons")
@@ -32,11 +33,16 @@ def touch_icons():
         icons.append('<link rel="icon" type="image/png" sizes="%ix%i"  href="%s">' % (icon_size, icon_size, icon_url))
 
     icons.append(
-        '<meta name="msapplication-TileImage" content="%s">' % os.path.join(icon_path, "favicon-%ix%i.png" % (144, 144))
+        '<meta name="msapplication-TileImage" content="%s">' % url_join(icon_path, "favicon-%ix%i.png" % (144, 144))
     )
+    icons.append('<meta name="msapplication-TileColor" content="%s">' % getattr(settings, "TILE_COLOR", "#FFFFFF"))
+    icons.append('<meta name="theme-color" content="%s">' % getattr(settings, "THEME_COLOR", "#FFFFFF"))
 
     favicon_url = url_join(icon_path, "favicon.ico")
     icons.append('<link rel="shortcut icon" href="%s" type="image/x-icon">' % favicon_url)
     icons.append('<link rel="icon" href="%s" type="image/x-icon">' % favicon_url)
+
+    manifest_url = url_join(icon_path, "manifest.json")
+    icons.append('<link rel="manifest" href="%s">' % manifest_url)
 
     return mark_safe("\n".join(icons))
--- favorites_icons/templatetags/utils.py
+++ favorites_icons/templatetags/utils.py
@@ -1,13 +1,15 @@
+import json
 import os
 import re
 
 from PIL import Image
 from django.conf import settings
+from django.contrib.sites.models import Site
 
 
 def url_join(*args):
     os_path = os.path.join(*args)
-    path_parts = re.split(r'/|\\', os_path)
+    path_parts = re.split(r"/|\\", os_path)
 
     return "/".join(path_parts)
 
@@ -41,3 +43,51 @@ def generate_icons(**kwargs):
             if overwrite or not os.path.exists(target_file):
                 img.thumbnail((icon_size, icon_size), Image.ANTIALIAS)
                 img.save(target_file, "PNG")
+
+
+def generate_manifest():
+    static_root = getattr(settings, "STATIC_ROOT", False)
+    static_url = getattr(settings, "STATIC_URL", False)
+
+    icon_path = os.path.join(static_root, "favicons")
+    icon_url = os.path.join(static_url, "favicons")
+
+    target_file = os.path.join(icon_path, "manifest.json")
+
+    icon_sizes = getattr(
+        settings, "ICON_SIZES", [16, 32, 57, 60, 64, 72, 76, 96, 114, 120, 144, 152, 180, 192, 256, 512]
+    )
+
+    site_name = False
+    current_site = False
+
+    if hasattr(settings, "SITE_ID"):
+        current_site = Site.objects.get_current()
+
+    if hasattr(settings, "SITE_NAME"):
+        site_name = getattr(settings, "SITE_NAME")
+
+    elif not site_name and isinstance(current_site, Site):
+        site_name = current_site.name
+
+    if not site_name:
+        site_name = "My app"
+
+    manifest = {"name": site_name, "icons": []}
+
+    density_factor = 48
+    for icon_size in icon_sizes:
+        icon_url = url_join(icon_url, "favicon-%ix%i.png" % (icon_size, icon_size))
+
+        manifest["icons"].append(
+            {
+                "src": icon_url,
+                "sizes": "%ix%i" % (icon_size, icon_size),
+                "type": "image/png",
+                "density": str(icon_size / density_factor),
+            }
+        )
+
+    with open(target_file) as manifest_file:
+        json.dump(manifest, manifest_file)
+        manifest_file.close()
